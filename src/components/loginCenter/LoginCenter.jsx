2b875e65ca2bc976aa3294f5f66ec55e3f7fe55f
fumifumi <satouabe30@gmail.com>
2022-09-20 22:48:46 +0900

    test

--- src/components/loginCenter/LoginCenter.jsx
+++ src/components/loginCenter/LoginCenter.jsx
@@ -3,14 +3,13 @@ import "./LoginCenter.css"
 export default function LoginCenter() {
   return (
       <div className='login_wapp'>
-        <div className='login_left'>
-              <div className='loginLogo'>
-              <div className = "loLText">
-                  <span className='loginLeftText'>Fumi</span>
-                  <span className='loginLeftText2'>SNS</span>
-              </div>
-              </div>
-        </div>
+        <div className = "login_left">
+            <div className = "loLText">
+
+                      <span className='loginLeftText'>Fumi</span>
+                      <span className='loginLeftText2'>SNS</span>
+          </div>
+          </div>
         <div className='login_right'>
           <div className='loRText'>
             <span className='loginRightText'>Login</span>
@@ -19,6 +18,10 @@ export default function LoginCenter() {
             <span className='loginPass'>PassWord</span>
             <input type  = "text" className = "loginPassBox"/>
             <span className='loginButton'>login</span>
+            <span className='loginButton2'>Create account</span>
+            <div className='rightEn'>
+              
+            </div>
             {/* <s pan className='loginPass'>PassWord</span> */}
         </div>
      </div>
